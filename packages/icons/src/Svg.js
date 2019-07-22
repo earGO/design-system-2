c786a0d98375f35310bf686d656b9cbaabe945da
Logvinov_fv <logvinov_fv@ursip.ru>
2019-07-22 17:31:39 +0300

    made icons components using svg, added stories, moving to cross-import to components #39

--- packages/icons/src/Svg.js
+++ packages/icons/src/Svg.js
@@ -1,13 +1,29 @@
-import styled from 'styled-components'
+import styled, {keyframes, css} from 'styled-components'
 import {space, color} from 'styled-system'
 
-const Svg = styled('svg')(
-  {
-    flex: 'none'
-  },
-  space,
-  color
-)
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const styles = css`
+  animation: ${rotate};
+  animation-duration: ${props => (props.spin ? '2s' : ' 0s')};
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+`
+
+const Svg = styled('svg')`
+  flex: none;
+  ${styles}
+  ${space}
+  ${color}
+`
 
 Svg.propTypes = {
   ...space.propTypes,
--- packages/icons/stories/Access.stories.js
+++ packages/icons/stories/Access.stories.js
@@ -3,7 +3,7 @@ import {storiesOf} from '@storybook/react'
 
 import Accessible from '../components/Accessible'
 
-storiesOf(`Access`, module)
+storiesOf(`Accessible`, module)
   .addParameters({
     info: {
       inline: true,
@@ -20,7 +20,28 @@ storiesOf(`Access`, module)
   .add('Default', () => <Accessible />, {
     info: {
       text: `
-          По умолчанию без параметров иконка выводится в виде знака вопроса. 
+         Иконку можно импортировать как самостоятельный компонент, использовав для импорта её имя - например Accessible. Без параметров она отобразится чёрным цветом 
+        `
+    }
+  })
+  .add('Colored', () => <Accessible color={'red'} />, {
+    info: {
+      text: `
+          Иконке можно передать цвет в параметр color. 
+        `
+    }
+  })
+  .add('Colored & Sized', () => <Accessible color={'red'} size={48} />, {
+    info: {
+      text: `
+          Размер иконки можно изменять параметром size, передавая размер в пикселях (без приставки px, в формате number). 
+        `
+    }
+  })
+  .add('Spinning', () => <Accessible size={48} spin />, {
+    info: {
+      text: `
+          Размер иконки можно изменять параметром size, передавая размер в пикселях (без приставки px, в формате number). 
         `
     }
   })
