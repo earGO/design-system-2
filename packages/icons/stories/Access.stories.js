6b44ec58e3535b5ecbf7cdadca09df7e62bc71f7
Logvinov_fv <logvinov_fv@ursip.ru>
2019-07-22 15:57:28 +0300

    added icons package, storybook is working, trying to make a compound component and test it #39

---
+++ packages/icons/stories/Access.stories.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+
+import Accessible from '../components/Accessible'
+
+storiesOf(`Access`, module)
+  .addParameters({
+    info: {
+      inline: true,
+      header: false,
+      styles: {
+        header: {
+          h1: {
+            color: 'red'
+          }
+        }
+      }
+    }
+  })
+  .add('Default', () => <Accessible />, {
+    info: {
+      text: `
+          По умолчанию без параметров иконка выводится в виде знака вопроса. 
+        `
+    }
+  })
