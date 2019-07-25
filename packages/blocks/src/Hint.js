872dd0e63ce618bd84ba76156a54c9098b35b627
Logvinov_fv <logvinov_fv@ursip.ru>
2019-07-25 17:06:33 +0300

    added Hint  component, need to write some tests #50

--- packages/blocks/src/Hint.js
+++ packages/blocks/src/Hint.js
@@ -1,16 +1,106 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Icon, Flex, Box, Text, Popover, Card} from '@design-system/components'
+import styled from 'styled-components'
 
-function Hint({...props}) {
-  return (
-    <div>
-      <p>Hint stateless</p>
-    </div>
-  )
+const ClickableIcon = styled(Box)`
+cursor: pointer;
+transition: all;
+transition-duration: {$props=>props.theme.duration.fast};
+transition-timing-function: {$props=>props.theme.timingFunction.easeInOut};
+&:hover{
+  transform: scale(1.0015);
+  color:  {$props=>props.theme.colors.primary};
+}
+`
+
+const HintCard = styled(Card)`
+  z-index: 3;
+  border-width: 0;
+`
+
+class Hint extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isPopoverOpen: false
+    }
+  }
+
+  render() {
+    const {isPopoverOpen} = this.state
+    const {
+      size,
+      arrowColor,
+      bgColor,
+      shiftLeft,
+      shiftTop,
+      hintText,
+      color,
+      position
+    } = this.props
+    return (
+      <Flex
+        width={400}
+        height={420}
+        flexDirection={'column'}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+        <Popover
+          isOpen={isPopoverOpen}
+          onClickOutside={() => this.setState({isPopoverOpen: false})}
+          position={position} // preferred position
+          content={({position, targetRect, popoverRect}) => (
+            <Popover.ArrowContainer
+              position={position}
+              targetRect={targetRect}
+              popoverRect={popoverRect}
+              arrowColor={arrowColor}
+              arrowSize={10}
+              arrowStyle={{opacity: 1.0, zIndex: 6}}
+            >
+              <HintCard p={3} bg={bgColor} boxShadowSize={'md'}>
+                <Text color={color}>{hintText}</Text>
+              </HintCard>
+            </Popover.ArrowContainer>
+          )}
+          contentLocation={({nudgedLeft, nudgedTop}) => ({
+            top: nudgedTop + shiftTop,
+            left: nudgedLeft + shiftLeft
+          })}
+        >
+          <ClickableIcon
+            onClick={() => this.setState({isPopoverOpen: !isPopoverOpen})}
+          >
+            <Icon name={'help_outline'} size={size} color={color} />
+          </ClickableIcon>
+        </Popover>
+      </Flex>
+    )
+  }
 }
 
-Hint.propTypes = {}
+Hint.propTypes = {
+  arrowColor: PropTypes.string,
+  bgColor: PropTypes.string,
+  hintText: PropTypes.string,
+  position: PropTypes.string,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  shiftTop: PropTypes.number,
+  shiftLeft: PropTypes.number
+}
 
-Hint.defaultProps = {}
+Hint.defaultProps = {
+  arrowColor: 'white',
+  bgColor: 'white',
+  hintText: 'Enter hint text',
+  position: 'top',
+  color: 'black',
+  size: 18,
+  shiftTop: 5,
+  shiftLeft: -10
+}
 
 export default Hint
--- packages/blocks/stories/Hint.stories.js
+++ packages/blocks/stories/Hint.stories.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
+import {Flex} from '@design-system/components'
+import theme from '@design-system/theme'
 
 import Hint from '../src/Hint'
 
@@ -17,10 +19,96 @@ storiesOf(`Hint`, module)
       }
     }
   })
-  .add('Default', () => <Hint />, {
-    info: {
-      text: `
-          По умолчанию без параметров иконка выводится в виде знака вопроса. 
+  .add(
+    'Default',
+    () => (
+      <Flex
+        width={200}
+        theme={theme}
+        height={150}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+        <Hint theme={theme} hintText={'This is a hint text'} />
+      </Flex>
+    ),
+    {
+      info: {
+        text: `
+          По умолчанию подсказка выводится с чёрной иконкой и чёрным текстом. 
         `
+      }
     }
-  })
+  )
+  .add(
+    'Colored',
+    () => (
+      <Flex
+        width={200}
+        theme={theme}
+        height={150}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+        <Hint
+          theme={theme}
+          hintText={'This is a hint text'}
+          color={'primary'}
+        />
+      </Flex>
+    ),
+    {
+      info: {
+        text: `
+          Подсказке можно передать цвет - окрасятся и иконка, и текст. 
+        `
+      }
+    }
+  )
+  .add(
+    'Positioned',
+    () => (
+      <Flex
+        width={200}
+        theme={theme}
+        height={150}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+        <Hint
+          theme={theme}
+          hintText={'This is a hint text'}
+          color={'primary'}
+          position={'right'}
+        />
+      </Flex>
+    ),
+    {
+      info: {
+        text: `
+          Подсказке можно передать позицию, например - появление справа. 
+        `
+      }
+    }
+  )
+  .add(
+    'Shifted',
+    () => (
+      <Flex
+        width={200}
+        theme={theme}
+        height={150}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+        <Hint theme={theme} hintText={'This is a hint text'} shiftLeft={-25} />
+      </Flex>
+    ),
+    {
+      info: {
+        text: `
+          Контейнер с подсказкой можно смещать относительно иконки. 
+        `
+      }
+    }
+  )
