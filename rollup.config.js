b3f59b059afd89924438f234d5bcc54ef109546d
earGO <rh.edwin@gmail.com>
2019-08-18 10:33:52 -0500

    done rollup build #28

--- rollup.config.js
+++ rollup.config.js
@@ -4,32 +4,50 @@ const commonjs = require('rollup-plugin-commonjs')
 const json = require('rollup-plugin-json')
 const external = require('rollup-plugin-peer-deps-external')
 const url = require('rollup-plugin-url')
-const peerDepsExternal = require('rollup-plugin-peer-deps-external')
+// const peerDepsExternal = require('rollup-plugin-peer-deps-external')
 
 // import { terser } from 'rollup-plugin-terser'
-const postcss = require( 'rollup-plugin-postcss')
+const postcss = require('rollup-plugin-postcss')
 
-const pkg =require( './package.json')
+const pkg = require('./package.json')
 
 module.exports = {
   input: 'src/index.js',
   output: [
     {
-      file: pkg.main,
+      file: 'dist/index.cjs.js',
       format: 'cjs',
       sourcemap: true
     },
     {
-      file: pkg.module,
+      file: 'dist/index.es.js',
       format: 'es',
       sourcemap: true
     }
   ],
-  external: ['styled-components', 'styled-normalize', 'styled-system'],
+  external: [
+    'styled-components',
+    'styled-normalize',
+    'react-input-autosize',
+    'stylis-rule-sheet',
+    'styled-system',
+    'prop-types',
+    'react',
+    'react-dom',
+    '@design-system/theme',
+    'react-tiny-popover',
+    'stylus-rule-sheet',
+    'add-dom-event-listener',
+    'rc-animate',
+    'classnames',
+    'moment',
+    'shallowequal',
+    'color'
+  ],
   // https://github.com/WebReflection/hyperHTML/issues/304#issuecomment-443950244
   context: 'null',
   plugins: [
-    peerDepsExternal(),
+    // peerDepsExternal(),
     external(),
     url({
       // by default, rollup-plugin-url will not handle font files
@@ -58,7 +76,7 @@ module.exports = {
       browser: true
     }),
     commonjs({
-      include: ['node_modules/**','../../node_modules/**','../../../node_modules/**'],
+      include: /node_modules/,
       exclude: ['node_modules/process-es6/**']
     }),
     json(),
