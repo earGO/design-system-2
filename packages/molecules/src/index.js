40ffa9135448078a5adebcc5146b9937a7676896
earGO <rh.edwin@gmail.com>
2019-09-02 04:53:43 -0500

    added ActionCell #71

--- packages/molecules/src/index.js
+++ packages/molecules/src/index.js
@@ -1,4 +1,5 @@
 export {default as Hint} from './Hint'
+export {default as ActionCell} from './ActionCell'
 export {default as LabeledIconButton} from './LabeledIconButton'
 export {default as DropdownMenu} from './DropdownMenu'
 export {default as DropdownMenuButton} from './DropdownMenuButton'
