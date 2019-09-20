531aee00358b6e38f1dd0553be3c80357c605a7e
Иванов-Тельманов Антон Серге <ivanov-telmanov@mdi.ru>
2019-09-20 14:28:06 +0300

    collapse changed into functional component #87

--- packages/atoms/src/Collapse.js
+++ packages/atoms/src/Collapse.js
@@ -1,51 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import propTypes from 'prop-types'
 import Box from './Box'
 import CollapsePanel from './CollapsePanel'
 
 /** Используется, если необходимо сгруппировать или скрыть большое количество содержимого. */
-export class Collapse extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      activeKeys: this.props.activeKeys || this.props.defaultActiveKeys || []
-    }
+
+export function Collapse({activeKeys, defaultActiveKeys, onChange, children}) {
+  const [activeKeysFromState, setActiveKeysFromState] = useState(
+    activeKeys || defaultActiveKeys || []
+  )
+
+  const handleChange = newActiveKeys => {
+    setActiveKeysFromState(newActiveKeys)
+    typeof onChange === 'function' && onChange(newActiveKeys)
   }
 
-  handleChange = newActiveKeys => {
-    this.setState({
-      activeKeys: newActiveKeys
-    })
-    this.props.onChange && this.props.onChange(newActiveKeys)
-  }
-
-  onPanelClick = panelKey => {
-    const {activeKeys} = this.state
-    let newActiveKeys = [...activeKeys]
-    if (activeKeys.includes(panelKey)) {
+  const onPanelClick = panelKey => {
+    let newActiveKeys = [...activeKeysFromState]
+    if (activeKeysFromState.includes(panelKey)) {
       // Closed the panel
-      newActiveKeys = activeKeys.filter(key => key !== panelKey)
+      newActiveKeys = activeKeysFromState.filter(key => key !== panelKey)
     } else {
       // Opened the panel
-      newActiveKeys = [...activeKeys, panelKey]
+      newActiveKeys = [...activeKeysFromState, panelKey]
     }
-    this.handleChange(newActiveKeys)
+    handleChange(newActiveKeys)
   }
 
-  getChildren = () => {
-    if (this.props.children !== null && this.props.children !== undefined) {
-      const {children} = this.props
+  const getChildren = () => {
+    if (children !== null && children !== undefined) {
       if (children.length) {
         return children.map((child, i) => {
           let key = child.key || String(i)
           let props = {
             key: child.key || String(i),
             panelKey: key,
-            togglePanel: this.onPanelClick,
+            togglePanel: onPanelClick,
             // If controlled, props should be the source of truth.
-            isOpen: this.props.activeKeys
-              ? this.props.activeKeys.includes(key)
-              : this.state.activeKeys.includes(key)
+            isOpen: activeKeys
+              ? activeKeys.includes(key)
+              : activeKeysFromState.includes(key)
           }
           return React.cloneElement(child, props)
         })
@@ -56,18 +50,16 @@ export class Collapse extends React.Component {
           panelKey: key,
           togglePanel: this.onPanelClick,
           // If controlled, props should be the source of truth.
-          isOpen: this.props.activeKeys
-            ? this.props.activeKeys.includes(key)
-            : this.state.activeKeys.includes(key)
+          isOpen: activeKeys
+            ? activeKeys.includes(key)
+            : activeKeysFromState.includes(key)
         }
         return React.cloneElement(children, props)
       }
     } else return null
   }
 
-  render() {
-    return <Box>{this.getChildren()}</Box>
-  }
+  return <Box>{getChildren()}</Box>
 }
 
 Collapse.propTypes = {
